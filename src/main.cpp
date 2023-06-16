847b6aa6ada6b2a63a2134b934b251c561fdf4d1
ianafp <wodejinlun@outlook.com>
2023-06-16 22:52:15 +0800

    add color mode

--- src/main.cpp
+++ src/main.cpp
@@ -7,25 +7,80 @@
 #include<vector>
 #include<unordered_set>
 
-std::string file_folder = "../img/baby/";
-std::string file_name = "baby.png";
+std::string file_folder = "../img/beihaiting/";
+std::string file_name = "beihaiting.jfif";
 
+void ImageChannelProcess(cv::Mat& HalftoneMat,cv::Mat& QrMat,cv::Mat& ImpMat,cv::Mat& ResMat,std::unordered_set<int>& necessary_idx)
+{
+    // preprocess
+    std::vector<module> modules;
+    std::vector<int> label;
+    GetModules(HalftoneMat,QrMat,ImpMat,modules);
+
+    // swap optimization
+    OptimizeBySwap(QrMat.rows,QrMat.cols,modules,label);
+
+    // fix error module
+    int index = 0;
+    int error_module = 0; // 指的是那些中心颜色和原二维码不一致的module
+    ResMat = HalftoneMat;
+    for(int i=0;i < QrMat.rows; i += 3)
+    {
+        for(int j=0; j < QrMat.cols; j += 3)
+        {
+            int copy = QrMat.at<uchar>(i + 1, j + 1);
+            if ( necessary_idx.find((i/3)*(QrMat.cols/3) + j/3) != necessary_idx.end() ){
+                index++;
+                for(int m=0;m<3;++m)
+                {
+                    for(int n=0;n<3;++n)
+                    {
+                        ResMat.at<uchar>(i+m,j+n) = QrMat.at<uchar>(i+m,j+n);
+                    }
+                }
+            }
+            else{
+                ConvertIdToPattern(ResMat,i,j,label[index++]);
+            }
+
+            if (QrMat.at<uchar>(i + 1, j + 1) != copy)
+                error_module++;
+                ResMat.at<uchar>(i + 1, j + 1) = copy;
+        }
+    }
+    std::cout << "error modules: " << error_module << std::endl;
+}
 int main(void)
 {
-    cv::Mat original = cv::imread(file_folder + file_name,cv::ImreadModes::IMREAD_GRAYSCALE);
-    cv::Mat halftone;
-    cv::resize(original,original,cv::Size(111,111),0.0,0.0,cv::INTER_CUBIC);
-    cvHalftone(&original,&halftone);
-    std::cout<<original.rows<<", "<<original.cols<<std::endl;
-    cv::imwrite(file_folder + "halftone_cpp_pre_resize.png",halftone);
-    cv::Mat importance;
-    original = cv::imread(file_folder + file_name);
-    cv::resize(original,original,cv::Size(111,111),0.0,0.0,cv::INTER_CUBIC);
-    GetBorderImg(original, importance);
-    cv::imwrite(file_folder + "importance_cpp_pre_resize.png",importance);
-
-    cv::Mat qrcode;
+    // version
     int version = 5;
+    int image_size = ((version-1)*4 + 21) * 3;
+    // split to channels
+    cv::Mat original = cv::imread(file_folder + file_name,-1);
+    
+    // resize
+    cv::resize(original,original,cv::Size(111,111),0.0,0.0,cv::INTER_CUBIC);
+    // get channel counts
+    int channel_counts = original.channels();
+    cv::Mat* channels = new cv::Mat[channel_counts];
+    // split
+    cv::split(original,channels);
+    // test merge
+    // cv::Mat test_merge;
+    // cv::merge(channels,channel_counts,test_merge);
+    // cv::imwrite(file_folder+"test_merge.png",test_merge);
+    for(int i=0;i<channel_counts;++i)
+    {
+        cv::imwrite(file_folder+"channels_origin_"+std::to_string(i)+".png",channels[i]);
+    }
+
+    // get importance map
+    cv::Mat importance;
+    GetBorderImg(original, importance);
+    cv::imwrite(file_folder + "importance.png",importance);
+
+    // get qr code 
+    cv::Mat qrcode;
     char qr_str[11] = "YuuuuSir!";
     std::unordered_set<int> necessary_idx;
     std::vector<mRSblock*> QR_rs_blocks;
@@ -41,36 +96,22 @@ int main(void)
     std::cout << "necessary modules size is: " << necessary_idx.size() << std::endl;
     cv::imwrite(file_folder + "qr.png", qrcode);
 
-    std::cout << "input" << std::endl;
-    std::cout << halftone.rows << "," << halftone.cols << std::endl;
-    std::cout << qrcode.rows << "," << qrcode.cols << std::endl;
-    std::cout << importance.rows << "," << importance.cols << std::endl;
-
-    std::vector<module> modules;
-    std::vector<int> label;
-    GetModules(halftone,qrcode,importance,modules);
-    OptimizeBySwap(halftone.rows,halftone.cols,modules,label);
-
-    int index = 0;
-    int error_module = 0; // 指的是那些中心颜色和原二维码不一致的module
-    for(int i=0;i < qrcode.rows; i += 3)
+    // handle each channel
+    for(int i = 0; i < channel_counts; ++i)
     {
-        for(int j=0; j < qrcode.cols; j += 3)
-        {
-            int copy = qrcode.at<uchar>(i + 1, j + 1);
-            if ( necessary_idx.find((i/3)*(qrcode.cols/3) + j/3) != necessary_idx.end() ){
-                index++;
-            }
-            else{
-                ConvertIdToPattern(qrcode,i,j,label[index++]);
-            }
+        // get channels halftone img
+        cv::Mat halftone;
+        cv::resize(channels[i],channels[i],cv::Size(image_size,image_size),0.0,0.0,cv::INTER_CUBIC);
+        cvHalftone(channels+i,channels+i);
+        cv::imwrite(file_folder + "halftone_channel_"+std::to_string(i)+".png",channels[i]);
+        ImageChannelProcess(channels[i],qrcode,importance,channels[i],necessary_idx);
+        cv::imwrite(file_folder + "res_channel_"+std::to_string(i)+".png",channels[i]);
+    } 
+    // merge channels 
+    cv::Mat result;
+    cv::merge(channels,channel_counts,result);
+    // save
+    cv::imwrite(file_folder+"res_qr.png",result);
+    // std::cout << "input" << std::endl;
 
-            if (qrcode.at<uchar>(i + 1, j + 1) != copy)
-                error_module++;
-                qrcode.at<uchar>(i + 1, j + 1) = copy;
-        }
-    }
-    std::cout << "error modules: " << error_module << std::endl;
-    cv::imwrite(file_folder + "res_qr.png",qrcode);
-    std::cout << std::endl;
 }
